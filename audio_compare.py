99ed1b8a8388a1707975d8aa5d61afbd3ab46b23
Mateusz Kalinowski <mateusz.kalinowski@arm.com>
2023-01-23 21:36:26 +0100

    Calculate Fourier Transform for reference and original file

--- audio_compare.py
+++ audio_compare.py
@@ -3,8 +3,17 @@
 # brew install openblas lapack
 import librosa
 
-y, sr = librosa.load('wol_original.wav')
-# 3. Run the default beat tracker
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+y_ref, sr_ref = librosa.load('wol_original.wav')
+y_comp, sr_comp = librosa.stft('wol_9db_q1_boost.wav')
 
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+# 3. Run the default beat tracker
+# tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+# print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+
+S_ref = librosa.stft(y=y_ref)
+S_comp = librosa.stft(y=y_comp)
+print(S_ref)
+print(S_comp)
+
+# calculate Mean Squared Error for both matrices
