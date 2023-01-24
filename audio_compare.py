34c461b955a2bcda943c67dab5d8e43740619170
Mateusz Kalinowski <mateusz.kalinowski@arm.com>
2023-01-24 02:49:57 +0100

    Count Mean Square Error of two audio tracks

--- audio_compare.py
+++ audio_compare.py
@@ -2,18 +2,23 @@
 # !!! INSTALL LAPACK AND OPENBLAS NEEDED FOR LIBROSA:
 # brew install openblas lapack
 import librosa
+import numpy as np
 
 y_ref, sr_ref = librosa.load('wol_original.wav')
-y_comp, sr_comp = librosa.stft('wol_9db_q1_boost.wav')
+y_comp, sr_comp = librosa.load('wol_original.wav')
 
 # 3. Run the default beat tracker
 # tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
 # print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
-S_ref = librosa.stft(y=y_ref)
-S_comp = librosa.stft(y=y_comp)
-print(S_ref)
-print(S_comp)
+# get rid of phase, reduce complex numbers to natural numbers
+# https://stackoverflow.com/questions/61600977/can-someone-help-me-understand-the-np-abs-conversion-for-stft-in-librosa
+S_ref = np.abs(librosa.stft(y=y_ref))
+S_comp = np.abs(librosa.stft(y=y_comp))
 
 # calculate Mean Squared Error for both matrices
+print(S_ref)
+MSE = np.square(np.subtract(S_ref,S_comp)).mean()
+
+print(MSE)
